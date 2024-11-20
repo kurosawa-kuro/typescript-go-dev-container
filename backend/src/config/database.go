485b3eb8187fa55d386b5a3b39123a8ecc8fd687
Toshifumi Kurosawa <kuromailserver@gmail.com>
2024-11-20 14:25:15 +0000

    aaaaaaaa

--- backend/src/config/database.go
+++ backend/src/config/database.go
@@ -12,7 +12,7 @@ import (
 
 func SetupDB() *gorm.DB {
 	// 環境変数名を.envファイルに合わせる
-	host := os.Getenv("DATABASE_HOST")
+	host := os.Getenv("DOCKER_DATABASE_HOST")
 	user := os.Getenv("POSTGRES_USER")
 	password := os.Getenv("POSTGRES_PASSWORD")
 	dbname := os.Getenv("POSTGRES_DB")
